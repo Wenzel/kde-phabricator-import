36646ccc9f6203847f5c9cd335e10ea7460a6d89
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-02-16 22:51:53 +0100

    update CONDUIT token and add certificate

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ DB_HOST = "172.17.0.2" # my docker container address
 DB_USER = "admin"
 DB_PASS = "admin"
 CONDUIT_HOST = "http://127.0.0.1:8081"
-CONDUIT_TOKEN = "api-o5frysweuakh3occyf2ezw4dtvhb"
+CONDUIT_TOKEN = "api-ri7ktadp3rotohvvudhv5m2rsgpk"
+CONDUIT_CERT = "ojuaulwfogie5pglou7p5omggobuh2hyh4tb5htj7mub2nex7rzyatbvuemy6p7ydcxcqat4ib66sncavmp5zqm2r76efqjgy2faovuqq4bj3bvbzlipwmyrs5daypfngsnnvbiqg4tnoeft22ifbxpezc7gjdhmfwlho7jecilxvgmorgx2qnxitwnafaco4vzlaqltpdliahxcjnnejmoy6uvefld7kkk5kg5cr64tjw52dtuxdadfwynaawf"
