028acc9ecde3ace23bb0a3e67502e217f2315f3f
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-02-18 15:51:25 +0100

    add SQLAlchemy test code

--- test.py
+++ test.py
@@ -7,11 +7,26 @@ import re
 import pdb
 
 # local
+from tools.wmfphablib import phabdb
 import config
+from kanboard import Kanboard
 
 # 3rd
 from phabricator import Phabricator
-from tools.wmfphablib import phabdb
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Table
+from sqlalchemy.orm import sessionmaker
+
+
+# global
+engine = create_engine('sqlite:///db.sqlite', echo=True)
+Base = declarative_base(engine)
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'autoload':True}
 
 def main():
     db = phabdb.phdb(host=config.DB_HOST,
@@ -21,8 +36,20 @@ def main():
 
     phab = Phabricator(host=config.CONDUIT_HOST,
                        token=config.CONDUIT_TOKEN)
-    phab.update_interfaces()
-    phab.user.whoami()
+    # phab.update_interfaces()
+    # phab.maniphest.createtask(title="title",
+    #                          description="full desc")
+
+
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # queries
+    tasks = session.query(Task).all()
+    for t in tasks:
+        print(t.title)
+
 
 
 if __name__ == '__main__':
