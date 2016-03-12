685bf488342472fa05fe248aaba7dae859c83eb2
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-03-12 04:23:46 +0100

    update config

--- python/config.py
+++ python/config.py
@@ -11,11 +11,13 @@ DB_USER = "admin"
 DB_PASS = "admin"
 DB_NAME = "default_project"
 
+PHAB_HOST = "http://172.17.0.3"
+PHAB_ADMIN = 'admin'
+
 SECRET_KEY = "my_secret_key"
 SECRET_PATH = hashlib.sha1(SECRET_KEY.encode('utf-8')).hexdigest()
-PHAB_HOST = 'http://172.17.0.3:4000/'
-PHAB_SECRET_URL = PHAB_HOST + SECRET_PATH + '/'
-PHAB_ADMIN = 'admin'
+
+ADD_USER_URL = PHAB_HOST + ':4000' + '/' + SECRET_PATH + '/'
 
 CONDUIT_HOST = "http://127.0.0.1:8081/api/"
 CONDUIT_TOKEN = "api-txmxzqtgwfxgcgeoiru4aidamcy2"
